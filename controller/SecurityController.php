b62672e56ea67764a79849301459ca9a98ff41c5
timfmb09 <tim.fmb@gmail.com>
2022-11-14 16:44:07 +0100

    Bug Login/Regiter

--- controller/SecurityController.php
+++ controller/SecurityController.php
@@ -29,27 +29,31 @@
                 $userManager->add(["nickname"=>$nickname, "email"=>$email, "password"=>$passwordHash]);
         
 
-            return ["view" => VIEW_DIR."forum/home.php"];
+            return ["view" => VIEW_DIR."view/security/register.php"];
 
         }
 
-        //Connexion
-        public function login($iduser){
-            $userManager = new UserManager();
-            $email = filter_input(INPUT_POST, "nickname", FILTER_SANITIZE_EMAIL);
-            $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        // //Connexion
+        // public function login($iduser){
+        //     $userManager = new UserManager();
+        //     $email = filter_input(INPUT_POST, "nickname", FILTER_SANITIZE_EMAIL);
+        //     $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
             
-            if 
+        //     if($email){
+
+        //         $userManager->getPassword($password) ->getUser($email);
+        //         $password = password_verify("password"=>$password);
+
+        //     if($password){
+
+
+        //     }    
+    
+        //     }
             
+        //     return ["view" => VIEW_DIR."view/security/login.php"];
 
-
-
-
-
-
-            return ["view" => VIEW_DIR."view/security/login.php"];
-
-        }
+        // }
 
 
 
