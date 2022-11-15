6df7dae4d09ddc1508256fef04bbaa238c72c823
timfmb09 <tim.fmb@gmail.com>
2022-11-15 16:55:27 +0100

    Bug Login&register

--- controller/SecurityController.php
+++ controller/SecurityController.php
@@ -14,9 +14,6 @@
 
         public function index(){}
 
-
-
-
         // Creation of an user
         public function addUser(){
             $userManager = new UserManager();
@@ -39,53 +36,12 @@
                 $this->redirectTo('security', 'login');
             }
 
-
-            
+           
         }
 
 
 
-
-        //     if (!empty($_POST)) {
-        //         foreach ($_POST as $key => $value) { // Nettoyage des données
-        //             $post[$key] = trim(strip_tags($value)); // récupération du _POST dans un tableau
-        //         }
-        //     if(strlen($post['nickname']) < 2 || strlen($post['nickname']) > 50){ // on défini les propriétés de 'nickname'
-        //       $errors[] = '<div class="alert alert-danger" role="alert">Votre pseudo doit comporter entre 2 et 50 caractères</div>';
-        //         }
-                 
-        //     if(!isset($post['password']) && !empty($post['password'])) {
-        //       $errors[] = '<div class="alert alert-danger" role="alert">Votre mot de passe n\'est pas valide</div>';
-        //       }
-              
-        //     if(count($errors) > 0){ // On compte les erreurs, si il y en as (supérieur a 0), on passera la variable $showErrors à true.
-        //             $showErrors = true; // valeur booleen // permettra d'afficher nos erreurs s'il y en a
-        //             $nickname = $post['nickname'];
-        //             $password = $post['password'];
-        //     }
-
-        //     else {
-        //         // On sécurise notre password en le hashant
-        //          // IMPORTANT : On ne stocke jamais de mot de passe en clair en pdo
-        //         $password = password_hash($post['password'], PASSWORD_DEFAULT);
-        //     }
-      
-        //     if($res->execute()){
-        //         $success = true; // Pour afficher le message de réussite si tout est bon
-        //     }
-        //     else {
-        //         die(var_dump($res->errorInfo()));
-        //         }
-        //     }
-            
-        //     return [
-        //         "view" => VIEW_DIR."view/security/register.php"
-        //            ];
-                       
-
-        // }
-
-        // //Connexion
+        //Connexion
         public function login(){
             // $userManager = new UserManager();
             // $email = filter_input(INPUT_POST, "nickname", FILTER_SANITIZE_EMAIL);
@@ -108,21 +64,7 @@
         }
 
         public function register(){
-            // $userManager = new UserManager();
-            // $email = filter_input(INPUT_POST, "nickname", FILTER_SANITIZE_EMAIL);
-            // $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-            
-            // if($email){
-
-            //     $userManager->getPassword($password) ->getUser($email);
-            //     $password = password_verify("password"=>$password);
-
-            // if($password){
-
-
-            // }    
-    
-            // }
+       
             
             return ["view" => VIEW_DIR."security/register.php"];
 
--- view/security/register.php
+++ view/security/register.php
@@ -1,14 +1,7 @@
 <h1 class="text-center">Ajouter un utilisateur</h1>
 <br>
 <div class="container">
-      <?php
-      if($success == 'true' && $success == 'true'){ // On affiche la réussite si tout fonctionne
-        echo '<div class="alert alert-success" role="alert"> L\'utilisateur est bien créer ! </div>';
-      }
-      if($showErrors){
-        echo implode('<br>', $errors);
-      }
-      ?>
+
       <form method="post" class="pure-form pure-form-aligned" action='index.php?ctrl=security&action=addUser'>
          <div class="input-group">
             <span class="input-group-addon" id="basic-addon1">Nickname</span>
