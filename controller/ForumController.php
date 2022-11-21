7b10336469593daa2f60f9234708e57034e40ee7
Timfmb09 <tim.fmb@gmail.com>
2022-11-21 16:23:55 +0100

    MAJ List PostByUser Done

--- controller/ForumController.php
+++ controller/ForumController.php
@@ -71,7 +71,7 @@
                 // la méthode "findAll" est une méthode générique qui provient de l'AbstractController (dont hérite chaque controller de l'application)
                 "data" => [
                     "posts" => $posts,
-                    "topic" => $topicManager->findOneByUser($id)
+                    "topic" => $topicManager->findOneById($id)
                 ]
             ];
 
